6ef740dded3d596a0dd19927acb923bdc2cf1603
EmirlanDogdurbaev <jigit38@gmail.com>
2021-04-08 15:26:42 +0300

    fixed errors end change

--- src/components/PharmacyBuilder/PharmacyControls/PharmacyControls.js
+++ src/components/PharmacyBuilder/PharmacyControls/PharmacyControls.js
@@ -1,18 +1,32 @@
-import PharmacyControl from "./PharmacyControl/PharmacyControl";
 import classes from "./PharmacyControls.module.css";
+import PharmacyControl from "./PharmacyControl/PharmacyControl";
+import Button from "../../UI/Backdrop/Button/Button";
 
-const PharmacyControls = ({ ingredients, addIngredient, removeIngredient }) => {
+
+const PharmacyControls = ({addIngredient, removeIngredient }) => {
   const result = [];
+  const ingredients = [
+    "loperamid",
+    "trimol",
+    "mezim",
+    "askorbinka",
+    "dimidrol",
+    "laktoG",
+  ];
   for (const ingredient in ingredients) {
     result.push(
       <PharmacyControl
-        type={ingredient}
+        key={ingredients[ingredient]}
+        type={ingredients[ingredient]}
         addIngredient={addIngredient}
         removeIngredient={removeIngredient}
       />
     );
   }
-  return <div className={classes.PharmacyControls}>{result}</div>;
+  return <div className={classes.PharmacyControls}>
+    {result}
+    <Button disabled={!ingredients.length}>Order</Button>
+    </div>;
 };
 
 export default PharmacyControls;
