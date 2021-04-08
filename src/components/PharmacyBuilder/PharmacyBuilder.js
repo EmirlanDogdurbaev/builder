10dd40a8d48746b1bea94023281366d7b11f7ba6
EmirlanDogdurbaev <jigit38@gmail.com>
2021-04-08 15:23:20 +0300

    add Price end fixed errors

--- src/components/PharmacyBuilder/PharmacyBuilder.js
+++ src/components/PharmacyBuilder/PharmacyBuilder.js
@@ -1,35 +1,53 @@
-import React from "react";
-import classes  from "./PharmacyBuilder.module.css"
-import PharmacyControls from "./PharmacyControls/PharmacyControls"
-import PharmacyPreview from "./PharmacyPreview/PharmacyPreview"
-import {useState} from "react"
+import React, { useState, useEffect } from "react";
+import classes from "./PharmacyBuilder.module.css";
+import PharmacyControls from "./PharmacyControls/PharmacyControls";
+import PharmacyPreview from "./PharmacyPreview/PharmacyPreview";
+import axios from "axios";
 
 const PharmacyBuilder = () => {
-  const [ingredients, setIngredients] = useState({
-    askorbinka: 1,
-    trimol : 1,
-    dimidrol : 1,
-    lartoG : 1,
-    mezim: 1,
-    loperamid: 1,
-  });
+  const prices = {
+    trimol: 5,
+    loperamid: 4,
+    mezim: 5,
+    laktoG: 5,
+    dimidrol: 3,
+    askorbinka: 2,
+  };
+
+  const [ingredients, setIngredients] = useState([]);
+  const [price, setPrice] = useState(0);
+
+  useEffect(
+    () => axios
+      .get('https://builder-8c2e1-default-rtdb.firebaseio.com/default.json')
+      .then(response => {
+        setPrice(response.data.price);
+        setIngredients(Object.values(response.data.ingredients));
+        console.log(ingredients)
+      }),
+  []);
 
   function addIngredient(type) {
-    const newIngredients = { ...ingredients };
-    newIngredients[type]++;
+    const newIngredients = [...ingredients];
+    newIngredients.push(type);
+    setPrice(price + prices[type]);
     setIngredients(newIngredients);
   }
 
-  function removeIngredient(type) {
-    if (ingredients[type]) {
-      const newIngredients = { ...ingredients };
-    newIngredients[type]--;
+  function removeIngredient(type){ 
+    const newIngredients = [...ingredients]
+    const index = newIngredients.lastIndexOf(type);
+    if (index !== -1) {
+    newIngredients.splice(index, 1);
+    setPrice(price - prices[type]);
     setIngredients(newIngredients);
     }
   }
+
   return (
     <div className={classes.PharmacyBuilder}>
-      <PharmacyPreview ingredients={ingredients} />
+      <PharmacyPreview ingredients={ingredients}
+       price={price} />
       <PharmacyControls
         ingredients={ingredients}
         addIngredient={addIngredient}
@@ -40,3 +58,8 @@ const PharmacyBuilder = () => {
 };
 
 export default React.memo(PharmacyBuilder);
+
+
+
+
+
