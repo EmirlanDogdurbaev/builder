482159a1b31c40f85cc8e4045786c1729cda0d33
EmirlanDogdurbaev <jigit38@gmail.com>
2021-04-08 15:28:44 +0300

    change size picture

--- src/components/PharmacyBuilder/PharmacyPreview/PharmacyPreview.js
+++ src/components/PharmacyBuilder/PharmacyPreview/PharmacyPreview.js
@@ -1,22 +1,23 @@
-  import Medicine from "../Medicine/Medicine";
-  import classes from "./PharmacyPreview.module.css";
+import Medicine from "../Medicine/Medicine";
+import classes from "./PharmacyPreview.module.css";
 
 const PharmacyPreview = ({ ingredients }) => {
-    const result =[];
+  const result = [];
 
-    for (const ingredient in ingredients) {
-        for (let i = 0; i < ingredients[ingredient]; i++) {
-            result.push(<Medicine type = {ingredient} />)
-        }
-    }
-
-    return ( 
-        <div className = {classes.PharmacyPreview}>
-            <div className={classes.ingredients}>
-                {result}
-            </div>
-        </div>
+  for (const ingredient in ingredients) {
+    result.push(
+      <Medicine
+        key={ingredients[ingredient] + ingredient}
+        type={ingredients[ingredient]}
+      />
     );
-}
- 
+  }
+
+  return (
+    <div className={classes.PharmacyPreview}>
+      <div className={classes.ingredients}>{result}</div>
+    </div>
+  );
+};
+
 export default PharmacyPreview;
