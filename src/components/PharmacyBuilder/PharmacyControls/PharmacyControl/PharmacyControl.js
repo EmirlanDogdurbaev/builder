501d86f5b86e2c94210c66cc1cf48f75508f8752
EmirlanDogdurbaev <jigit38@gmail.com>
2021-04-08 15:28:13 +0300

    change structure

--- src/components/PharmacyBuilder/PharmacyControls/PharmacyControl/PharmacyControl.js
+++ src/components/PharmacyBuilder/PharmacyControls/PharmacyControl/PharmacyControl.js
@@ -1,16 +1,15 @@
+import Button from "../../../UI/Backdrop/Button/Button";
 import Medicine2 from "../../Medicine2/Medicine2";
 import classes from "./PharmacyControl.module.css";
 
 const PharmacyControl = ({ type, addIngredient, removeIngredient }) => {
-    return ( 
-        <div className = {classes.PharmacyControl}>
-            <button className = {classes.ControlButton} onClick = {() => removeIngredient(type)}>-</button>
-            <Medicine2  type = {type} >
-                
-            </Medicine2>
-            <button className = {classes.ControlButton} onClick = {() => addIngredient(type)}>+</button>
-        </div>
-    );
-}
- 
+  return (
+    <div className={classes.PharmacyControl}>
+      <Button onClick={() => removeIngredient(type)}>-</Button>
+      <Medicine2 type={type} />
+      <Button onClick={() => addIngredient(type)}>+</Button>
+    </div>
+  );
+};
+
 export default PharmacyControl;
