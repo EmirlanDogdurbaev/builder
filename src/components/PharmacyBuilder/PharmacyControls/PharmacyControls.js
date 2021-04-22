8ca702abea42ea6cdcc8cd5d8be0120c7a996a83
EmirlanDogdurbaev <jigit38@gmail.com>
2021-04-22 20:47:23 +0300

    changed the contennt of the code in PharmacyControls

--- src/components/PharmacyBuilder/PharmacyControls/PharmacyControls.js
+++ src/components/PharmacyBuilder/PharmacyControls/PharmacyControls.js
@@ -1,32 +1,34 @@
+import Button from "../../UI/Button/Button";
+import PharmacyControl from "./PharmacyControl/PharmacyControl"
 import classes from "./PharmacyControls.module.css";
-import PharmacyControl from "./PharmacyControl/PharmacyControl";
-import Button from "../../UI/Backdrop/Button/Button";
 
-
-const PharmacyControls = ({addIngredient, removeIngredient , startOrdering }) => {
-  const result = [];
-  const ingredients = [
-    "loperamid",
-    "trimol",
-    "mezim",
-    "askorbinka",
-    "dimidrol",
-    "laktoG",
-  ];
+const PharmacyControls = ({
+  ingredients,
+  addIngredient,
+  removeIngredient,
+  startOrdering,
+  type
+}) => {
+  const results = [];
+  let total = 0;
   for (const ingredient in ingredients) {
-    result.push(
-      <PharmacyControl
-        key={ingredients[ingredient]}
-        type={ingredients[ingredient]}
-        addIngredient={addIngredient}
-        removeIngredient={removeIngredient}
-      />
-    );
+    total += ingredients[ingredient];
+    results.push(<PharmacyControl
+        key={ingredient}
+        add={addIngredient}
+        remove={removeIngredient}
+        count={ingredients[ingredient]}
+        type={ingredient} />)
   }
-  return <div className={classes.PharmacyControls}>
-    {result}
-    <Button disabled={!ingredients.length}  onClick ={startOrdering} aqua >Order</Button>
-    </div>;
-};
+
+  return (
+    <div className={classes.PharmacyControls}>
+     
+      <strong>Ingredients</strong>
+      {results}
+      <Button disabled={!total} onClick={startOrdering} order>Order</Button>
+    </div>
+  );
+}
 
 export default PharmacyControls;
