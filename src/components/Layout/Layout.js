7685c8424703e50b6d21ab6d7fafc1f5146c7802
EmirlanDogdurbaev <jigit38@gmail.com>
2021-04-22 22:37:12 +0300

    added new tekst

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -3,9 +3,8 @@ import Toolbar from "../Toolbar/Toolbar";
 import Drawer from "../Drawer/Drawer"
 
 import classes from "./Layout.module.css";
-import PharmacyBuilder from "../PharmacyBuilder/PharmacyBuilder";
 
-const Layout = ({ children }) => {
+const Layout = ({children}) => {
   const [drawerOpen, setDrawerOpen] = useState(false);
 
   return (
@@ -13,7 +12,8 @@ const Layout = ({ children }) => {
       <Toolbar openDrawer={() => setDrawerOpen(true)} />
       <Drawer open={drawerOpen} closeDrawer={() => setDrawerOpen(false)} />
       <main>
-      <PharmacyBuilder />
+       <p>Pharmacy builder by Emirlan Dogdurbaev</p> 
+      {children}
       </main>
     </div>
   );
