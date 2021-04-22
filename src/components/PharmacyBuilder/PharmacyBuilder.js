1ddd4d913d3ecfeea497def23720e5ee6af0fdef
EmirlanDogdurbaev <jigit38@gmail.com>
2021-04-22 20:27:56 +0300

    changed the content of the code PharmacyBuilder.js

--- src/components/PharmacyBuilder/PharmacyBuilder.js
+++ src/components/PharmacyBuilder/PharmacyBuilder.js
@@ -1,51 +1,53 @@
-import React, { useState, useEffect } from "react";
 import classes from "./PharmacyBuilder.module.css";
 import PharmacyControls from "./PharmacyControls/PharmacyControls";
 import PharmacyPreview from "./PharmacyPreview/PharmacyPreview";
-import axios from "axios";
 import Modal from "../UI/Modal/Modal";
+import OrderSummary from "./OrderSummary/OrderSummary";
+import Button from "../UI/Button/Button";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
-const PharmacyBuilder = () => {
+const PharmacyBuilder = ({ history }) => {
   const prices = {
-    trimol: 5,
-    loperamid: 4,
-    mezim: 5,
-    laktoG: 5,
-    dimidrol: 3,
-    askorbinka: 2,
+    askorbinka: 15,
+    trimol:12,
+    dimidrol:14,
+    loperamid:17,
+    mezim:19,
+    laktoG:21,
   };
-
-  const [ingredients, setIngredients] = useState([]);
+  const [ingredients, setIngredients] = useState({});
   const [price, setPrice] = useState(0);
   const [ordering, setOrdering] = useState(false);
 
-  useEffect(
-    () => axios
+  useEffect(loadDefaults, []);
+
+  function loadDefaults() {
+    axios
       .get('https://builder-8c2e1-default-rtdb.firebaseio.com/default.json')
       .then(response => {
         setPrice(response.data.price);
-        setIngredients(Object.values(response.data.ingredients));
-      }),
-
-  []);
+        setIngredients(response.data.ingredients);
+      });
+  }
 
   function addIngredient(type) {
-    const newIngredients = [...ingredients];
-    newIngredients.push(type);
+    const newIngredients = { ...ingredients };
+    newIngredients[type]++;
     setPrice(price + prices[type]);
     setIngredients(newIngredients);
   }
 
-  function removeIngredient(type){ 
-    const newIngredients = [...ingredients]
-    const index = newIngredients.lastIndexOf(type);
-    if (index !== -1) {
-    newIngredients.splice(index, 1);
-    setPrice(price - prices[type]);
-    setIngredients(newIngredients);
+  function removeIngredient(type) {
+    if (ingredients[type]) {
+      const newIngredients = { ...ingredients };
+      newIngredients[type]--;
+      setPrice(price - prices[type]);
+      setIngredients(newIngredients);
     }
   }
 
+ 
   function startOrdering() {
     setOrdering(true);
   }
@@ -54,27 +56,46 @@ const PharmacyBuilder = () => {
     setOrdering(false);
   }
 
+  function finishOrdering() {
+    axios
+    .post('https://builder-8c2e1-default-rtdb.firebaseio.com/orders.json',{
+      ingredients: ingredients,
+      price: price,
+      address: "Shopokova kv 4",
+      phone:"0707379480",
+      name:"Keremet Kerimova",
+    })
+    .then(() =>{
+      setOrdering(false);
+      loadDefaults();
+      history.push('/checkout')
+    })
+  }
+
   return (
     <div className={classes.PharmacyBuilder}>
-      <PharmacyPreview ingredients={ingredients}
-       price={price} />
+      <PharmacyPreview
+        ingredients={ingredients}
+        price={price} />
       <PharmacyControls
         ingredients={ingredients}
         addIngredient={addIngredient}
         removeIngredient={removeIngredient}
         startOrdering={startOrdering}
-      />
-       <Modal
-            ingredients = {ingredients}
-                show={ordering}
-                cancel={stopOrdering}></Modal>
+        />
+      <Modal
+        show={ordering}
+        cancel={stopOrdering}>
+        <OrderSummary
+          ingredients={ingredients}
+          price={price}
+            />
+          <Button onClick={finishOrdering} green>Checkout</Button>
+          <Button onClick={stopOrdering}>Cancel</Button>
+        </Modal>
+        
     </div>
   );
-};
+}
 
-export default React.memo(PharmacyBuilder);
-
-
-
-
-
+export default PharmacyBuilder;
