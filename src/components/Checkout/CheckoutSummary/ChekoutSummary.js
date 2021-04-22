d33a43f6c46de68e0dc28af31fcc0d0ec52b98fc
EmirlanDogdurbaev <jigit38@gmail.com>
2021-04-22 22:35:49 +0300

    added new ingredients

--- src/components/Checkout/CheckoutSummary/ChekoutSummary.js
+++ src/components/Checkout/CheckoutSummary/ChekoutSummary.js
@@ -6,14 +6,16 @@ import Button from "../../UI/Button/Button"
 const CheckoutSummary = ({ cancelCallback }) => {
   return (
     <div className={classes.CheckoutSummary}>
-      <div>
+      <div className={classes.check}>
         <PharmacyPreview ingredients={{
-           
-
-        
+           askorbinka:1,
+           trimol:1,
+           dimidrol:1,
+           mezim:1,
+           loperamid:1,
+           laktoG:1,
         }} price={150} />
-      </div>
-      <div>
+        
         <Button>Checkout</Button>
         <Button onClick={cancelCallback}>Cancel</Button>
       </div>
