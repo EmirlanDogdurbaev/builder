f17eab6c0ab8637115dd93c15774e28c351956a6
EmirlanDogdurbaev <jigit38@gmail.com>
2021-05-04 22:01:48 +0300

    added the necessary lines of code

--- src/components/PharmacyBuilder/PharmacyPreview/PharmacyPreview.js
+++ src/components/PharmacyBuilder/PharmacyPreview/PharmacyPreview.js
@@ -1,29 +1,28 @@
 import Medicine from "../Medicine/Medicine";
 import classes from "./PharmacyPreview.module.css";
-import test from "../../../images/karTest.png";
+
+
 
 const PharmacyPreview = ({ ingredients, price }) => {
   const result = [];
   for (const ingredient in ingredients) {
+  
+    
     for (let i = 0; i < ingredients[ingredient]; i++) {
-      result.push(<Medicine key={ingredient + i} type={ingredient} />);
+      result.push(<Medicine key={ingredient + i} type={ingredient} />)
     }
   }
 
   return (
     <div className={classes.PharmacyPreview}>
-    <div className={classes.korobka}
-      
-  >
-      <div
-        className={classes.ingredients}
-        style={{ backgroundImage: `url(${test})`, backgroundSize: "cover" , width:"400px"}}>
-        {result}
+      <div className={classes.medical}>
+        <div className={classes.container}>
+          {result}
+        </div>
       </div>
+      <div className={classes.price}>{price.toFixed(1)} som</div>
     </div>
-    <div className={classes.price}>{price.toFixed(1)} som</div>
-  </div>
   );
-};
+}
 
-export default PharmacyPreview;
+export default PharmacyPreview; 
