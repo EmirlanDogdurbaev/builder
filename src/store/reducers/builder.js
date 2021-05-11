61ea43baf03214044f1c3345b2ec1de4911a8c2b
EmirlanDogdurbaev <jigit38@gmail.com>
2021-05-11 21:04:50 +0300

    created builder/reducer

---
+++ src/store/reducers/builder.js
@@ -0,0 +1,39 @@
+import { ADD_MEDICAL, REMOVE_MEDICAL, SET_MEDICALS } from "../actions/types";
+
+const initialState = {
+  medicals: {},
+  price: 0,
+};
+const prices = {
+  vitA: 5,
+  vitB: 5,
+  vitC: 5,
+  vitD: 5,
+  vitE: 5,
+  vitK: 5,
+};
+
+const builder = (state = initialState, action) => {
+  const newState = { ...state };
+  switch (action.type) {
+    case ADD_MEDICAL:
+      newState.medicals[action.medical]++;
+      newState.price += prices[action.medical];
+      break;
+    case REMOVE_MEDICAL:
+      newState.medicals[action.medical]--;
+      newState.price -= prices[action.medical];
+      break;
+    case SET_MEDICALS:
+      newState.medicals = action.data.medicals;
+      newState.price = action.data.price;
+      break;
+
+    default:
+      break;
+  }
+
+  return newState;
+};
+
+export default builder;
