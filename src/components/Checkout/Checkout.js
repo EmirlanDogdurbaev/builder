c181c22b81266673b613c0ba962f28ebba092fb6
EmirlanDogdurbaev <jigit38@gmail.com>
2021-05-11 20:43:41 +0300

    using useSelector

--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -1,41 +1,45 @@
-import classes from "./Checkout.module.css";
-import CheckoutForm from "./CheckoutForm/CheckoutForm";
-import axios from "axios";
 import PharmacyPreview from "../PharmacyBuilder/PharmacyPreview/PharmacyPreview";
+import CheckoutForm from "./CheckoutForm/CheckoutForm";
+import classes from "./Checkout.module.css";
+import axios from "../../axios";
+import { useSelector } from "react-redux";
+import withAxios from "../withAxios";
 
 const Checkout = ({ history }) => {
+  const medicals = useSelector((state) => state.builder.medicals);
+  const price = useSelector((state) => state.builder.price);
+
   function cancelCallback() {
-    history.replace('/');
+    history.replace("/");
   }
+
   function submitCallback(event) {
     const data = new FormData(event.target);
 
-    axios.post('https://builder-8c2e1-default-rtdb.firebaseio.com/orders.json', {
-      name: data.get('name'),
-      address: data.get('address'),
-      phone: data.get('phone'),
-      ingredients: {
-
-      },
-      price: 100,
-    }).then(response => {
-      history.replace('/');
-    });
+    axios
+      .post("/orders.json", {
+        name: data.get("name"),
+        address: data.get("address"),
+        phone: data.get("phone"),
+        medicals: medicals,
+        price: price,
+      })
+      .then((response) => {
+        history.replace("/");
+      });
 
     event.preventDefault();
   }
+
   return (
     <div className={classes.Checkout}>
-      <PharmacyPreview ingredients={{
-        vitA: 5,
-        vitB: 5,
-        vitC: 5,
-        vitD: 5,
-        vitE: 5,
-      }} price={150} />
-      <CheckoutForm cancelCallback={cancelCallback} submitCallback={submitCallback} />
+      <PharmacyPreview medicals={medicals} price={price} />
+      <CheckoutForm
+        cancelCallback={cancelCallback}
+        submitCallback={submitCallback}
+      />
     </div>
   );
-}
+};
 
-export default Checkout;
+export default withAxios(Checkout, axios);
