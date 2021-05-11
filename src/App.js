917f2f51124a688fd5734c74384bab77120f87f7
EmirlanDogdurbaev <jigit38@gmail.com>
2021-05-11 20:40:33 +0300

    imported Orders

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import Layout from "./components/Layout/Layout"
+import Layout from "./components/Layout/Layout";
 import Checkout from "./components/Checkout/Checkout";
 
 import { Redirect, Route, Switch } from "react-router";
 import PharmacyBuilder from "./components/PharmacyBuilder/PharmacyBuilder";
 
 import "./App.css";
+import Orders from "./components/Orders/Orders";
 
 const App = () => {
   return (
     <div className="App">
-     <Layout>
+      <Layout>
         <Switch>
           <Route path="/" component={PharmacyBuilder} exact />
           <Route path="/checkout" component={Checkout} />
+          <Route parh="./orders" component={Orders} />
           <Redirect to="/" />
         </Switch>
       </Layout>
-       
     </div>
   );
-}
+};
 
 export default App;
