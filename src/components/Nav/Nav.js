f43451ba467bd99da9991f0e4bf2a5651aecab1a
EmirlanDogdurbaev <jigit38@gmail.com>
2021-05-11 20:53:20 +0300

    added Orders

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -4,10 +4,11 @@ import NavItem from "./NavItem/NavItem";
 const Nav = () => {
   return (
     <ul className={classes.Nav}>
-      <NavItem url="/" exact>Builder</NavItem>
-      <NavItem url="/checkout">Checkout</NavItem>
+      <NavItem url="/" exact>Builder </NavItem>
+      {/* <NavItem url="/checkout">Checkout</NavItem> */}
+      <NavItem url="/orders">Orders</NavItem>
     </ul>
   );
-}
+};
 
 export default Nav;
